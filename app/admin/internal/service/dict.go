534e4ad297d1358db9195a6d04c4e96f5ff79072
honlyc <hedan806@gmail.com>
2024-04-06 23:41:44 +0800

    feat: add dict manage

--- app/admin/internal/service/dict.go
+++ app/admin/internal/service/dict.go
@@ -6,6 +6,8 @@ import (
 	"github.com/go-atreus/atreus-server/app/admin/internal/data"
 	"github.com/go-kratos/kratos/v2/log"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"math/rand"
+	"strconv"
 )
 
 var _ dict.DictServer = (*DictService)(nil)
@@ -21,11 +23,39 @@ type DictService struct {
 }
 
 func (m *DictService) DictValidHash(context.Context, *emptypb.Empty) (*dict.ValidHashResp, error) {
-	return &dict.ValidHashResp{Results: []string{"hash"}}, nil
+	return &dict.ValidHashResp{Results: []string{strconv.Itoa(rand.Int())}}, nil
 }
 
 func (m *DictService) DictData(ctx context.Context, in *dict.DictDataReq) (*dict.DictDataResp, error) {
 	switch in.DictCodes {
+	case "role_type":
+		return &dict.DictDataResp{Results: []*dict.DictData{
+			{
+				DictCode: "role_type",
+				DictItems: []*dict.SysDictDataItem{
+					{Value: "1", Name: "系统", Id: 1, Attributes: &dict.SysDictItemAttributes{TagColor: "orange"}},
+					{Value: "2", Name: "自定义", Id: 2, Attributes: &dict.SysDictItemAttributes{TagColor: "green"}},
+				}},
+		}}, nil
+	case "gender":
+		return &dict.DictDataResp{Results: []*dict.DictData{
+			{
+				DictCode: "gender",
+				DictItems: []*dict.SysDictDataItem{
+					{Value: "1", Name: "男", Id: 1},
+					{Value: "2", Name: "女", Id: 2},
+					{Value: "3", Name: "未知", Id: 3},
+				}},
+		}}, nil
+	case "user_status":
+		return &dict.DictDataResp{Results: []*dict.DictData{
+			{
+				DictCode: "user_status",
+				DictItems: []*dict.SysDictDataItem{
+					{Value: "1", Name: "正常", Id: 1},
+					{Value: "2", Name: "锁定", Id: 2},
+				}},
+		}}, nil
 	case "dict_value_type":
 		return &dict.DictDataResp{Results: []*dict.DictData{
 			{
